8a0e6cb2c45af2f303b9c3c15e591461ca708637
marcos <marcos.vinicius.dev3@gmail.com>
2026-02-28 17:33:29 -0300

    initial commit

---
+++ src/main/java/org/jacorreu/JaCorreuApplication.java
@@ -0,0 +1,13 @@
+package org.jacorreu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JaCorreuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JaCorreuApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/jacorreu/JaCorreuApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jacorreu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JaCorreuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
