db1cd398bc3aaa034324f1957daadd34ea665c1b
marcos <marcos.vinicius.dev3@gmail.com>
2026-03-08 10:53:15 -0300

    feat: create structure for RenewTokenUseCase

---
+++ src/main/java/org/jacorreu/identity/application/usecase/RenewTokenUseCase.java
@@ -0,0 +1,9 @@
+package org.jacorreu.identity.application.usecase;
+
+public class RenewTokenUseCase {
+    //TODO: Procurar o token fornecido.
+    //TODO: Verificar se está expirado ou revogado.
+    //TODO: Verificar se o utilizador dono do token ainda tem a conta ativa.
+    //TODO: Revogar o token atual.
+    //TODO: Gerar e guardar um novo par de tokens
+}
