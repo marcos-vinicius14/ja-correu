e9c86c4dbb2f59b8dff5f86ff1955cd536f8f694
marcos <marcos.vinicius.dev3@gmail.com>
2026-03-08 10:53:00 -0300

    feat: finish CustomUserDetailsUseCase

---
+++ src/main/java/org/jacorreu/identity/application/usecase/CustomUserDetailsUseCase.java
@@ -0,0 +1,32 @@
+package org.jacorreu.identity.application.usecase;
+
+import org.jacorreu.user.core.domain.UserDomain;
+import org.jacorreu.user.core.domain.valueobjects.Email;
+import org.jacorreu.user.gateway.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsUseCase implements UserDetailsService {
+
+    private final UserRepository repository;
+
+    public CustomUserDetailsUseCase(UserRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        UserDomain user = repository.findByEmail(Email.restore(email))
+                .orElseThrow(() -> new UsernameNotFoundException("Usuario nao encontrado!"));
+
+        return org.springframework.security.core.userdetails.User.builder()
+                .username(user.getEmail().getValue())
+                .password(user.getPassword().getValue())
+                .roles("USER")
+                .build();
+     }
+}
+
